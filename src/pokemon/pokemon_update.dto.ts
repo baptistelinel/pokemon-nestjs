26b86f6a383573a580da59949b302ea122304aa2
Baptiste LINEL <AJ1034869@tdf.hubtotal.net>
2021-10-02 01:40:32 +0200

    Update pokemon

--- src/pokemon/pokemon_update.dto.ts
+++ src/pokemon/pokemon_update.dto.ts
@@ -1,15 +1,19 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsString, IsOptional } from 'class-validator';
 
 export class PokemonUpdateDto {
   @IsNumber()
+  @IsOptional()
   id: number;
 
   @IsString()
+  @IsOptional()
   name: string;
 
   @IsNumber()
+  @IsOptional()
   height: number;
 
   @IsNumber()
+  @IsOptional()
   weight: number;
 }
