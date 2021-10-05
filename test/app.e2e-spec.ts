053dbf8d3ecde2865598cb8f7d35d6ba6a09fa44
Baptiste LINEL <AJ1034869@tdf.hubtotal.net>
2021-10-05 17:28:36 +0200

    e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PokemonCreateDto } from 'src/pokemon/pokemon_create.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,5 +21,22 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/pokemon (GET) success', () => {
+    return request(app.getHttpServer()).get('/pokemon').expect(200);
+  });
+
+  it('/pokemon (POST)', () => {
+    const postedPokemon: PokemonCreateDto = {
+      id: 19,
+      name: 'Tortank',
+      height: 100,
+      weight: 200,
+    };
+    return request(app.getHttpServer())
+      .post('/pokemon')
+      .send(postedPokemon)
+      .expect(201);
   });
 });
